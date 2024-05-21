aa51266e3eca3effcf38579eb15310daa872192b
itsjackie00 <jackiedanso33@gmail.com>
2024-05-21 18:45:39 +0200

    added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,16 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<header>
+    <div class="container d-flex justify-content-between align-items-center">
+        <a href="/">
+            <div id="logo">
+                <img class="img-fluid" src="/img/dc-logo.png" alt="Logo" />
+            </div>
+        </a>
+        <ul class="d-flex">
+            @foreach ($headerMenu as $menu)
+                <li>
+                    <a class="d-flex align-items-center text-uppercase p-2" href="{{ $menu['url'] }}">{{ $menu['name'] }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</header>
